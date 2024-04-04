92c9c02bfb3fbc42497514a72c6c0f1fa2d0dfea
EddieWebDev <eddandersson@gmail.com>
2024-04-04 21:26:48 +0200

    added env support

--- Pages/Test.tsx
+++ Pages/Test.tsx
@@ -1,9 +1,12 @@
 import { Text, View } from "react-native";
+import { ENV_VAR } from "@env";
 
-const Test = ({route}: any) => {
+const Test = ({ route }: any) => {
   return (
     <View>
-      <Text>This is {route.params.name} page</Text>
+      <Text>
+        This is {route.params.name} page and secret is: {ENV_VAR}
+      </Text>
     </View>
   );
 };
